de19aca3bb0193fab95d52be1610b4bb2dbcded7
sugondesenoots <martinmangahas972005@gmail.com>
2024-06-10 12:26:33 +0800

    Last COMMIT
    -App is working
    -Issues with Library deletion (No fixes online with new Unity Hub)
    -Packages used: Stylized Crystals & Apartment Kit & AR Foundation (Asset references)

---
+++ P03_Mangahas Kim Martin Cuaresma_2201260C_MXRT_Assignment2/Assets/Brick Project Studio/_BPS Basic Assets/Common/Scripts and Animations/Doors/opencloseDoor.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace SojaExiles
+
+{
+	public class opencloseDoor : MonoBehaviour
+	{
+
+		public Animator openandclose;
+		public bool open;
+		public Transform Player; 
+
+		public Canvas completionCanvas; //Reference to canvas for completion 
+		public Button closeApp; //Reference to close app button
+
+		void Start()
+		{
+			open = false; 
+			completionCanvas.gameObject.SetActive(false);
+		}
+
+		void OnMouseOver()
+		{
+			{
+				if (Player)
+				{
+					float dist = Vector3.Distance(Player.position, transform.position);
+					if (dist < 15)
+					{
+						if (open == false)
+						{
+							if (Input.GetMouseButtonDown(0))
+							{
+								StartCoroutine(opening());
+							}
+						}
+						else
+						{
+							if (open == true)
+							{
+								if (Input.GetMouseButtonDown(0))
+								{
+									StartCoroutine(closing());
+								}
+							}
+
+						}
+
+					}
+				}
+
+			}
+
+		}
+
+		IEnumerator opening()
+		{
+			print("you are opening the door");
+			openandclose.Play("Opening");
+			open = true;
+			yield return new WaitForSeconds(.5f); 
+			completionCanvas.gameObject.SetActive(true);
+		}
+
+		IEnumerator closing()
+		{
+			print("you are closing the door");
+			openandclose.Play("Closing");
+			open = false;
+			yield return new WaitForSeconds(.5f);
+		}
+		 
+		public void WaitForQuitClick()
+		{
+			closeApp.onClick.AddListener(Quit); 
+		} 
+
+        public void Quit()
+        {
+            //Closes the app
+            Application.Quit();
+
+            //If running in the editor, stop playing the scene
+#if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+#endif
+        }
+
+    }
+}
---
+++ P03_Mangahas Kim Martin Cuaresma_2201260C_MXRT_Assignment2/Assets/Scripts/InventorySystem/Item.cs
@@ -0,0 +1,14 @@
+[System.Serializable]
+public class Item
+{
+    public string name;
+    public int count;
+
+    public Item(string name, int count)
+    {
+        this.name = name;
+        this.count = count;
+    }
+}
+
+//Reference: https://www.youtube.com/watch?v=D39J3HLb7ow&list=LL&index=14&t=2s
